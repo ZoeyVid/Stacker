478e23086aa728260ac0eb1d0c8e0100d57f78fe
Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2022-02-17 19:39:35 +0100

    Update main.java

--- src/main/java/dev/sancraft/stacker/main.java
+++ src/main/java/dev/sancraft/stacker/main.java
@@ -80,7 +80,7 @@ public final class main extends JavaPlugin {
             Bukkit.getConsoleSender().sendMessage(main.getPrefix() + ChatColor.WHITE + "Language file loaded! (" + loadConfig.language() + ")");
         } else {
             try {
-                FileUtils.copyURLToFile(new URL("https://raw.githubusercontent.com/SanCraftDev/Stacker/master/src/main/languages/" + loadConfig.language() + ".yml"), new File("plugins/Stacker/language/" + loadConfig.language() + ".yml"));
+                FileUtils.copyURLToFile(new URL("https://raw.githubusercontent.com/SanCraftDev/Stacker/master/languages/" + loadConfig.language() + ".yml"), new File("plugins/Stacker/language/" + loadConfig.language() + ".yml"));
             } catch (Exception e) {
                 Bukkit.getConsoleSender().sendMessage(main.getPrefix() + ChatColor.RED + "Error while downloading language file!");
                 Bukkit.getServer().getPluginManager().disablePlugin(this);

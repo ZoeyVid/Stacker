53b5d40ad57996e5d56dd0ba1f8312c1d94143ca
David__Craft <48714689+DavidCraftDev@users.noreply.github.com>
2022-02-17 19:44:14 +0100

    Update main.java

--- src/main/java/dev/sancraft/stacker/main.java
+++ src/main/java/dev/sancraft/stacker/main.java
@@ -82,7 +82,6 @@ public final class main extends JavaPlugin {
             try {
                 FileUtils.copyURLToFile(new URL("https://raw.githubusercontent.com/SanCraftDev/Stacker/master/languages/" + loadConfig.language() + ".yml"), new File("plugins/Stacker/language/" + loadConfig.language() + ".yml"));
             } catch (Exception e) {
-                Bukkit.getConsoleSender().sendMessage(e);
                 Bukkit.getConsoleSender().sendMessage(main.getPrefix() + ChatColor.RED + "Error while downloading language file!");
                 Bukkit.getServer().getPluginManager().disablePlugin(this);
             }

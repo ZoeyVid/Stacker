85294669295c0a028b25662f1a8b6b7ae14bb10c
David <contact@davidcraft.de>
2023-03-27 14:26:16 +0000

    Gehts?
    Signed-off-by: GitHub <noreply@github.com>

--- src/main/java/de/zoeyvid/stacker/Listener/PlayerInteractListener.java
+++ src/main/java/de/zoeyvid/stacker/Listener/PlayerInteractListener.java
@@ -1,16 +1,23 @@
 package de.zoeyvid.stacker.Listener;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-
+import org.bukkit.util.Vector;
 
 public class PlayerInteractListener implements Listener {
 
     @EventHandler
     public void onPlayerInteractListener(PlayerInteractEvent event) {
         if (!(event.getAction() == Action.RIGHT_CLICK_AIR) && !(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
+        Player player = event.getPlayer();
+        if(!(player.isSneaking())) return;
+        Entity passenger = player.getPassengers().get(0);
+        passenger.eject();
+        passenger.setVelocity(new Vector(0,1.5,0));
     }
 
 }

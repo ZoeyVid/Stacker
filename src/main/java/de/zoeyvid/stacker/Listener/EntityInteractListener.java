e25385fb85bb15f9bdbc622160f2aa68d568d0b0
Zoey <zoey@z0ey.de>
2024-01-17 14:45:33 +0100

    Update EntityInteractListener.java
    Signed-off-by: Zoey <zoey@z0ey.de>

--- src/main/java/de/zoeyvid/stacker/Listener/EntityInteractListener.java
+++ src/main/java/de/zoeyvid/stacker/Listener/EntityInteractListener.java
@@ -4,10 +4,12 @@ import de.zoeyvid.stacker.loadConfig;
 import de.zoeyvid.stacker.loadLanguage;
 import de.zoeyvid.stacker.main;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.util.Vector;
 
 public class EntityInteractListener implements Listener {
 
@@ -22,7 +24,13 @@ public class EntityInteractListener implements Listener {
                 }
             } else {
                 if (player.getWorld().getPlayers().contains(target)) {
-                    player.addPassenger(target);
+                    ArmorStand placeholder = player.getWorld().spawn(player.getLocation(), ArmorStand.class);
+                    placeholder.setVisible(false);
+                    placeholder.setSmall(true);
+                    placeholder.setRemoveWhenFarAway(true);
+                    placeholder.addScoreboardTag("Stacker_Placeholder");
+                    placeholder.addPassenger(target);
+                    player.addPassenger(placeholder);
                 }
             }
         }
